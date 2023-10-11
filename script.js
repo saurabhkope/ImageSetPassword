df65f1666436ce2ebe218d30d8999459fc633817
saurabhkope <122842208+saurabhkope@users.noreply.github.com>
2023-10-11 08:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const convertBtn = document.getElementById('convertBtn');
     const downloadLink = document.getElementById('downloadLink');
 
-    convertBtn.addEventListener('click', () => {
+    convertBtn.addEventListener('click', async () => {
         const selectedFile = fileInput.files[0];
         const password = passwordInput.value;
 
         if (selectedFile && password) {
             const reader = new FileReader();
-            reader.onload = () => {
-                // You can use libraries like CryptoJS to encrypt the file here.
-                // For simplicity, let's just simulate a conversion.
-                const convertedData = `Password: ${password}\n\n${reader.result}`;
-                const convertedBlob = new Blob([convertedData], { type: 'text/plain' });
-                const url = window.URL.createObjectURL(convertedBlob);
+            reader.onload = async () => {
+                const imageBytes = reader.result;
+
+                // Create a PDF document
+                const pdfDoc = await PDFLib.PDFDocument.create();
+                const page = pdfDoc.addPage([600, 400]);
+                const contentStream = PDFLib.PDFContentStream.of(page);
+                contentStream.drawImage(await pdfDoc.embedPng(imageBytes), {
+                    x: 50,
+                    y: 150,
+                    width: 500,
+                    height: 200,
+                });
+                page.addContentStreams(contentStream);
+
+                // Encrypt the PDF with the password
+                const pdfBytes = await pdfDoc.save({ password });
+
+                // Create a Blob and provide a download link
+                const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+                const url = window.URL.createObjectURL(blob);
 
                 downloadLink.href = url;
                 downloadLink.style.display = 'block';
             };
 
-            reader.readAsDataURL(selectedFile);
+            reader.readAsArrayBuffer(selectedFile);
         }
     });
 });
